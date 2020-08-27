e419b97d15a3db714d7d2bbc87583aae4f40b9da
thetpainghtut <thetpainghtut.bf@gmail.com>
2020-08-27 16:48:33 +0630

    first commit

---
+++ src/components/Homework.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Homework Component</h2>
+  </div>
+</template>
+
+<script type="text/javascript">
+  
+</script>
+
+<style type="text/css">
+  
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+import { BootstrapVue, IconsPlugin , BIcon } from 'bootstrap-vue'
+
+// every components
+Vue.prototype.$http = axios 
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+// using b-icon component
+Vue.component('b-icon', BIcon)
+
+// css
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
